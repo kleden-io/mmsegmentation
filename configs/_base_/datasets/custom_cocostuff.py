7fbb6db0fabe0237d7eaddd13bc1b5e7711fba1c
Carlos Menacho <menachocarlos5@gmail.com>
2024-07-30 18:49:32 -0400

    Adding human segmentation config

--- configs/_base_/datasets/custom_cocostuff.py
+++ configs/_base_/datasets/custom_cocostuff.py
@@ -41,7 +41,7 @@ tta_pipeline = [
 ]
 train_dataloader = dict(
     batch_size=4,
-    num_workers=1,
+    num_workers=4,
     persistent_workers=True,
     sampler=dict(type='InfiniteSampler', shuffle=True),
     dataset=dict(
@@ -53,7 +53,7 @@ train_dataloader = dict(
         pipeline=train_pipeline))
 val_dataloader = dict(
     batch_size=1,
-    num_workers=1,
+    num_workers=4,
     persistent_workers=True,
     sampler=dict(type='DefaultSampler', shuffle=False),
     dataset=dict(
